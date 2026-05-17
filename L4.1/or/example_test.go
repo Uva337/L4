af34c9bca35d5f496dea288d5e4208ea7fa30eff
Uva337 <66168613+Uva337@users.noreply.github.com>
2026-05-17 19:54:41 +0300

    Update example_test.go

--- L4.1/or/example_test.go
+++ L4.1/or/example_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-// sig создает канал, который автоматически закроется через заданное время.
+// создает канал, который автоматически закроется через время
 func sig(after time.Duration) <-chan interface{} {
 	c := make(chan interface{})
 	go func() {
@@ -18,22 +18,17 @@ func sig(after time.Duration) <-chan interface{} {
 func ExampleOr() {
 	start := time.Now()
 
-	// Передаем несколько каналов с разным временем жизни.
-	// Самый быстрый сработает через 100 миллисекунд.
 	<-Or(
 		sig(2*time.Hour),
 		sig(5*time.Minute),
-		sig(100*time.Millisecond), // <-- Победитель
+		sig(100*time.Millisecond),
 		sig(1*time.Hour),
 		sig(1*time.Minute),
 	)
 
-	// Проверяем, что выполнение заняло доли секунды, а не часы
 	elapsed := time.Since(start)
 	if elapsed < 500*time.Millisecond {
 		fmt.Println("Успешно завершено по самому быстрому каналу!")
 	}
 
-	// Output:
-	// Успешно завершено по самому быстрому каналу!
 }
